01b2e83300b5767b9a2ac9d80e34e0d1cd96882d
Robert Anderson <rcanderson23@gmail.com>
2018-09-27 14:30:23 -0600

    Devel

--- config.sample.ps1
+++ config.sample.ps1
@@ -3,4 +3,19 @@ $username = "dbusername"
 $password = "dbpassword"
 $dbserverstring = "db1.domain.tld,1234"
 $database = "dbname"
-$connectionString = "Server=$dbserverstring;Database=$database;User ID=$username;Password=$password;encrypt=true;trustServerCertificate=true"
+$ConnectionString = "Server=$dbserverstring;Database=$database;User ID=$username;Password=$password;encrypt=true;trustServerCertificate=true"
+$sqlquery = "SELECT DISTINCT t.name as Term, p.Name as Period, c.name as Course, r.trialid as Trial, sch.schoolid as SchoolID, con.email as TeacherEmail, scon.email as StudentEmail
+               FROM Section s
+               JOIN SectionPlacement sp ON s.sectionid = sp.sectionID
+               JOIN Term t ON sp.termid = t.termid
+               JOIN Period p ON sp.periodid = p.periodid
+               JOIN Course c ON s.courseid = c.courseid
+               JOIN Roster r ON s.sectionid = r.sectionid
+               JOIN Person teacher ON s.teacherpersonid = teacher.personid
+               JOIN Contact con ON teacher.personid = con.personid
+               JOIN Person student ON r.personid = student.personid
+               JOIN Contact scon ON student.personid = scon.personid
+               JOIN Calendar cal ON c.calendarid = cal.calendarid
+               JOIN School sch ON cal.schoolid = sch.schoolid
+               JOIN Trial tr ON r.trialid = tr.trialid
+               WHERE tr.active = 1"
---
+++ examples/create_classes.ps1
@@ -0,0 +1,16 @@
+# Assumes Module is in same folder as your GAM scripts
+Import-Module "$PSScriptRoot\GAMFunctions" -Force
+
+# Source your config variables
+. "$PSScriptRoot\config.ps1"
+
+# Make your SQL query and output it into a CSV file named gamclassinfo.csv
+$sql_response = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query $sqlquery
+$sql_response | Export-Csv -path "$PSScriptRoot\example_gamclassinfo.csv" -NoTypeInformation -Header Term,Period,Course,Trial,SchoolID,TeacherEmail,StudentEmail,SectionID
+
+# Create Hashtable for all returned classes and store it
+# Get-Classes also accepts parameters for Term and SchoolID
+$classes = Get-Classes -InputFilePath "$PSScriptRoot\example_gamclassinfo.csv"
+
+# Create classes with GAM and set to Provisioned 
+Set-GAMClasses -Classes $classes -ClassState PROVISIONED -GAMAction CREATE

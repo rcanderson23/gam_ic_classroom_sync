c32671474994bb0b954fc37d5f23fa4c67a16a6a
Robert Anderson <rcanderson23@gmail.com>
2018-10-29 11:52:32 -0600

    remove header from exort-csv in example scripts

--- examples/sync_classes.ps1
+++ examples/sync_classes.ps1
@@ -6,7 +6,7 @@ Import-Module "$PSScriptRoot\GAMFunctions" -Force
 
 # Make your SQL query and output it into a CSV file named gamclassinfo.csv
 $sql_response = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query $sqlquery
-$sql_response | Export-Csv -path "$PSScriptRoot\example_gamclassinfo.csv" -NoTypeInformation -Header Term,Period,Course,Trial,SchoolID,TeacherEmail,StudentEmail,SectionID
+$sql_response | Export-Csv -path "$PSScriptRoot\example_gamclassinfo.csv" -NoTypeInformation 
 
 # Creates a hashtable of class rosters and stores it
 # You can modify this with the Term or SchoolID parameter
--- examples/update_classes.ps1
+++ examples/update_classes.ps1
@@ -6,7 +6,7 @@ Import-Module "$PSScriptRoot\GAMFunctions" -Force
 
 # Make your SQL query and output it into a CSV file named gamclassinfo.csv
 $sql_response = Invoke-Sqlcmd -ConnectionString $ConnectionString -Query $sqlquery
-$sql_response | Export-Csv -path "$PSScriptRoot\example_gamclassinfo.csv" -NoTypeInformation -Header Term,Period,Course,Trial,SchoolID,TeacherEmail,StudentEmail,SectionID
+$sql_response | Export-Csv -path "$PSScriptRoot\example_gamclassinfo.csv" -NoTypeInformation
 
 # Create Hashtable for all returned classes and store it
 $classes = Get-Classes -InputFilePath "$PSScriptRoot\example_gamclassinfo.csv"
